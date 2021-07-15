eaacb6d05ca02dd7445e89d5994c6f4c56adf20d
Marek Lhotecký <marek.lhotecky@marbes.cz>
2021-07-15 11:18:44 +0200

    add two enums to contants - STATUS for dynamic content according to api call phase and DATA_MESSAGE for display message about data according to api call phase

--- contants.tsx
+++ contants.tsx
@@ -1,1 +1,17 @@
+
+// api prefix - shorter definition in usages
 export const API_PREFIX = "https://jsonplaceholder.typicode.com";
+
+// enum for dynamic content according to api call phase
+export enum STATUS {
+    PENDING = "PENDING",
+    SUCCEEDED = "SUCCEEDED",
+    REJECTED = "REJECTED",
+}
+
+// enum for display message about data according to api call phase
+export enum DATA_MESSAGE {
+    LOADING = "Loading...",
+    NO_DATA = "No data.",
+    ERROR = "Error ocurred:"
+}
